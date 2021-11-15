b80a4bc9ad9d9c0df9ae1dd8cc7690753f757973
Tian Cai <33879882+BeatriceCai@users.noreply.github.com>
2021-11-14 23:13:54 -0500

    Add files via upload

---
+++ model_piple_utils.py
@@ -0,0 +1,289 @@
+import numpy as np
+import pandas as pd
+#--------------------------
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.nn import Sequential, ModuleList, Linear, ReLU, BatchNorm1d, Dropout, LogSoftmax
+from torch.nn.utils.rnn import pad_sequence
+#--------------------------
+from torch_geometric.data import DataLoader
+from torch_geometric.utils import to_dense_batch
+from rdkit import Chem
+#--------------------------
+from model_Yang import *
+from ligand_graph_features import *
+
+class EmbeddingTransform2(nn.Module):
+
+    def __init__(self, input_size, hidden_size, out_size,
+                 dropout_p=0.1):
+        super(EmbeddingTransform2, self).__init__()
+        self.dropout = nn.Dropout(p=dropout_p)
+        self.transform = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_size, out_size),
+            nn.BatchNorm1d(out_size)
+        )
+
+    def forward(self, embedding):
+        embedding = self.dropout(embedding)
+        hidden = self.transform(embedding)
+        return hidden
+class EmbeddingTransform(nn.Module):
+
+    def __init__(self, input_size, hidden_size, out_size,
+                 dropout_p=0.1):
+        super(EmbeddingTransform, self).__init__()
+        self.dropout = nn.Dropout(p=dropout_p)
+        self.transform = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_size, out_size),
+            nn.BatchNorm1d(out_size)
+        )
+
+    def forward(self, embedding):
+        embedding = self.dropout(embedding)
+        hidden = self.transform(embedding)
+        return hidden
+
+class AttentivePooling2(nn.Module):
+    """ Attentive pooling network according to https://arxiv.org/pdf/1602.03609.pdf """
+    def __init__(self,embedding_length = 300):
+        super(AttentivePooling2, self).__init__()
+        self.embedding_length = embedding_length
+        self.U = nn.Parameter(torch.zeros(self.embedding_length, self.embedding_length))
+
+    def forward(self, protein, ligand):
+        """ Calculate attentive pooling attention weighted representation and
+
+        """
+
+        U= self.U.expand(protein.size(0), self.embedding_length,self.embedding_length)
+        Q = protein
+        A = ligand
+        G = torch.tanh(torch.bmm(torch.bmm(Q, U), A.transpose(1,2)))
+        g_q = G.max(axis=2).values
+        g_a = G.max(axis=1).values
+
+        def get_attention_score(g_q,Q):
+            g_q_masked = g_q.masked_fill(g_q == 0, -1e9)
+            sigma_q = F.softmax(g_q_masked)
+            prot_repr = Q * sigma_q[:, :, None]
+            # prot_vec = prot_repr.sum(1)
+            # return sigma_q,prot_vec
+
+            return sigma_q, prot_repr
+
+        sigma_q, prot_vec = get_attention_score(g_q,Q)
+        sigma_a, chem_vec = get_attention_score(g_a,A)
+
+        return sigma_q, prot_vec, sigma_a, chem_vec
+class AttentivePooling(nn.Module):
+    """ Attentive pooling network according to https://arxiv.org/pdf/1602.03609.pdf """
+    def __init__(self, chem_hidden_size=128,prot_hidden_size=256):
+        super(AttentivePooling, self).__init__()
+        self.chem_hidden_size = chem_hidden_size
+        self.prot_hidden_size = prot_hidden_size
+        self.param = nn.Parameter(torch.zeros(chem_hidden_size, prot_hidden_size))
+
+    def forward(self, first, second):
+        """ Calculate attentive pooling attention weighted representation and
+        attention scores for the two inputs.
+
+        Args:
+            first: output from one source with size (batch_size, length_1, hidden_size)
+            second: outputs from other sources with size (batch_size, length_2, hidden_size)
+
+        Returns:
+            (rep_1, attn_1): attention weighted representations and attention scores
+            for the first input
+            (rep_2, attn_2): attention weighted representations and attention scores
+            for the second input
+        """
+        # logging.debug("AttentivePooling first {0}, second {1}".format(first.size(), second.size()))
+        param = self.param.expand(first.size(0), self.chem_hidden_size,self.prot_hidden_size)
+        # logging.debug("AttentivePooling params: {0}".format(param.size()))
+        wm1 = torch.tanh(torch.bmm(second,param.transpose(1,2)))
+        wm2 = torch.tanh(torch.bmm(first,param))
+        # logging.debug("Wm1 {}, Wm2 {} before softmax".format(wm1.size(),wm2.size()))
+        score_m1 = F.softmax(wm1,dim=2)
+        score_m2 = F.softmax(wm2,dim=2)
+        # logging.debug("score_m1 {}, score_m2 {}".format(score_m1.size(),score_m2.size()))
+        rep_first = first*score_m1
+        rep_second = second*score_m2
+        # logging.debug("AttentivePooling reps: {0}, {1}".format(rep_first.size(), rep_second.size()))
+
+        return ((rep_first, score_m1), (rep_second, score_m2))
+
+class distMtx_core_module(nn.Module):
+    def __init__(self,dim =20,num_class=3,
+                 feat_mode='attentive-pool'):
+        super(distMtx_core_module, self).__init__()
+        self.feat_mode=feat_mode
+        self.attpool = AttentivePooling2(embedding_length=dim)
+        self.ffn= torch.nn.Linear(dim,num_class)
+
+    def create_pairwise_embed(self,embed_a, embed_b, feat_mode=None):
+        if feat_mode == 'multiply':
+            feat = embed_a[:, :, None, :] * embed_b[:, None, :, :]
+        if feat_mode == 'attentive-pool':
+            sigma_a, a_vec, sigma_b, b_vec = self.attpool(embed_a, embed_b)
+            feat = a_vec[:, :, None, :] * b_vec[:, None, :, :]
+        return feat
+
+    def forward(self, embed_a, embed_b):
+        pairwise_feat = self.create_pairwise_embed(embed_a, embed_b, self.feat_mode)
+        logits = self.ffn(pairwise_feat)
+        return logits
+
+class interResidue_distMtx_classifier(nn.Module):
+    def __init__(self,dim =20,
+                 feat_mode='attentive-pool',pred_mode='binary',protein_descriptor=None,frozen='whole'):
+        super(interResidue_distMtx_classifier, self).__init__()
+        if pred_mode == 'binary':
+            num_class = 2
+        else:
+            num_class = 9 # 9 for even prob 0.1 setting,34 for PDNET
+        self.pred_DistMtx = distMtx_core_module(dim = dim,num_class=num_class,feat_mode=feat_mode)
+        self.protein_descriptor = protein_descriptor
+        self.frozen=frozen
+        if frozen =='encoder-whole':
+            print('frozen TAPE encoder whole')
+            for n, m in self.protein_descriptor.named_children():
+                if n == 'encoder':
+                    for param in m.parameters():
+                        param.requires_grad = False
+        if frozen=='encoder-attention':
+            print('frozen TAPE encoder attention layers')
+            for n, m in self.protein_descriptor.named_children():
+                if n == 'encoder':
+                    for n_layer, m_layer in m.named_children():
+                        for n_encoder, m_encoder in m_layer.named_children():
+                            for n_core, m_core in m_encoder.named_children():
+                                if n_core == 'attention':
+                                    # print('yes')
+                                    for param in m_core.parameters():
+                                        param.requires_grad = False
+    def forward(self,batch_input,device):
+        if self.frozen=='whole':
+            with torch.no_grad():
+                embed_full = self.protein_descriptor(batch_input['tokenized-padded'])[0][:,1:-1,:]# with beining and end tokens
+        else:
+            # print('updating..')
+            embed_full = self.protein_descriptor(batch_input['tokenized-padded'])[0][:, 1:-1,:]  # with beining and end tokens
+        embed_binding_site = torch.bmm(batch_input['binding site selection matrix'], embed_full)
+        logits = self.pred_DistMtx(embed_binding_site,embed_binding_site)
+        return  logits
+
+class DTI_distMtx_classifier(nn.Module):
+    def __init__(self,dim =20,
+                 feat_mode='attentive-pool',pred_mode='binary',protein_descriptor=None,chem_descriptor = None,
+                 frozen='whole',cwd=None,chem_pretrained='nope'):
+        super(DTI_distMtx_classifier, self).__init__()
+        if pred_mode == 'binary':
+            num_class = 2
+        else:
+            num_class = 9 # 9 for even prob 0.1 setting,34 for PDNET
+        self.pred_DistMtx = distMtx_core_module(dim = 300,num_class=num_class,feat_mode=feat_mode)
+        self.protein_descriptor = protein_descriptor
+        # self.chem_decriptor = GNN(num_layer=5,emb_dim=300,JK='last',drop_ratio=0.5,gnn_type='gin')
+        self.chem_decriptor = chem_descriptor
+        if chem_pretrained =='YES':
+            print('------------loading pretrained ContextPred on ChEMBLE alone')
+            contextpred_file='data/pretrained_contextpred/chemblFiltered_pretrained_model_with_contextPred.pth'
+            self.chem_decriptor.load_state_dict(torch.load(cwd+contextpred_file))
+        else:
+            print('----------- CotextPred unpretrained')
+        self.prot_transform = torch.nn.Linear(dim,300) # transform to same dim as contextpred
+        self.frozen=frozen
+        if frozen =='encoder-whole':
+            print('frozen TAPE encoder whole')
+            for n, m in self.protein_descriptor.named_children():
+                if n == 'encoder':
+                    for param in m.parameters():
+                        param.requires_grad = False
+        # if frozen=='encoder-attention':
+        #     print('frozen TAPE encoder attention layers')
+        #     for n, m in self.protein_descriptor.named_children():
+        #         if n == 'encoder':
+        #             for n_layer, m_layer in m.named_children():
+        #                 for n_encoder, m_encoder in m_layer.named_children():
+        #                     for n_core, m_core in m_encoder.named_children():
+        #                         if n_core == 'attention':
+        #                             # print('yes')
+        #                             for param in m_core.parameters():
+        #                                 param.requires_grad = False
+
+    def forward(self,batch_input,device):
+        # ---------------protein embedding -------------------
+        if self.frozen=='whole':
+            with torch.no_grad():
+                embed_full_prot = self.protein_descriptor(batch_input['tokenized-padded'])[0][:,1:-1,:]# with beining and end tokens
+        else:
+            # print('updating..')
+            embed_full_prot = self.protein_descriptor(batch_input['tokenized-padded'])[0][:, 1:-1,:]  # with beining and end tokens
+        embed_bs_prot = torch.bmm(batch_input['binding site selection matrix|prot'],embed_full_prot)
+        embed_bs_prot_T = self.prot_transform(embed_bs_prot)
+        # ---------------chemical embedding -------------------
+        chem_graphs_in = batch_input['chem graph loader']
+        embed_full_chem = self.chem_decriptor(chem_graphs_in.x, chem_graphs_in.edge_index,
+                              chem_graphs_in.edge_attr
+                              )
+        graph_repr_masked, mask_graph = to_dense_batch(embed_full_chem, chem_graphs_in.batch)
+        embed_bs_chem = torch.bmm(batch_input['binding site selection matrix|chem'], graph_repr_masked)
+        logits = self.pred_DistMtx(embed_bs_prot_T,embed_bs_chem)
+        return  logits
+
+class DTI_distMtx_classifier0(nn.Module):
+    def __init__(self,dim =20,
+                 feat_mode='attentive-pool',pred_mode='binary',protein_descriptor=None,chem_descriptor = None,
+                 frozen='whole',cwd=None,chem_pretrained='nope'):
+        super(DTI_distMtx_classifier0, self).__init__()
+        if pred_mode == 'binary':
+            num_class = 2
+        else:
+            num_class = 9 # 9 for even prob 0.1 setting,34 for PDNET
+        self.pred_DistMtx = distMtx_core_module(dim = 300,num_class=num_class,feat_mode=feat_mode)
+        self.protein_descriptor = protein_descriptor
+        self.chem_decriptor = GNN(num_layer=5,emb_dim=300,JK='last',drop_ratio=0.5,gnn_type='gin')
+        # self.chem_decriptor = chem_descriptor
+        if chem_pretrained =='YES':
+            print('------------loading pretrained ContextPred on ChEMBLE alone')
+            contextpred_file='data/pretrained_contextpred/chemblFiltered_pretrained_model_with_contextPred.pth'
+            self.chem_decriptor.load_state_dict(torch.load(cwd+contextpred_file))
+        else:
+            print('----------- CotextPred unpretrained')
+        self.prot_transform = torch.nn.Linear(dim,300) # transform to same dim as contextpred
+        self.frozen=frozen
+        if frozen =='encoder-whole':
+            print('frozen TAPE encoder whole')
+            for n, m in self.protein_descriptor.named_children():
+                if n == 'encoder':
+                    for param in m.parameters():
+                        param.requires_grad = False
+
+    def forward(self,batch_input,device):
+        # ---------------protein embedding -------------------
+        if self.frozen=='whole':
+            with torch.no_grad():
+                embed_full_prot = self.protein_descriptor(batch_input['tokenized-padded'])[0][:,1:-1,:]# with beining and end tokens
+        else:
+            # print('updating..')
+            embed_full_prot = self.protein_descriptor(batch_input['tokenized-padded'])[0][:, 1:-1,:]  # with beining and end tokens
+        embed_bs_prot = torch.bmm(batch_input['binding site selection matrix|prot'],embed_full_prot)
+        embed_bs_prot_T = self.prot_transform(embed_bs_prot)
+        # ---------------chemical embedding -------------------
+        chem_graphs_in = batch_input['chem graph loader']
+        embed_full_chem = self.chem_decriptor(chem_graphs_in.x, chem_graphs_in.edge_index,
+                              chem_graphs_in.edge_attr
+                              )
+        graph_repr_masked, mask_graph = to_dense_batch(embed_full_chem, chem_graphs_in.batch)
+        embed_bs_chem = torch.bmm(batch_input['binding site selection matrix|chem'], graph_repr_masked)
+        logits = self.pred_DistMtx(embed_bs_prot_T,embed_bs_chem)
+        return  logits
---
+++ utils_MAML.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import random
+from sklearn import metrics
+# --------------
+from transformers import BertTokenizer
+from transformers.configuration_albert import AlbertConfig
+from transformers.modeling_albert import AlbertForMaskedLM
+from transformers.modeling_albert import load_tf_weights_in_albert
+# --------------
+from data_tool_box import *
+# =======================================================
+#             data related
+# =======================================================
+def sample_balanced(s,k_spt,pfam_sampled,train_dict):
+    neg = train_dict[pfam_sampled]['neg_'+s].sample(k_spt,replace=True)
+    pos = train_dict[pfam_sampled]['pos_'+s].sample(k_spt,replace=True)
+    spt = pd.concat((neg,pos))
+    return spt.sample(frac=1)
+
+def sample_minibatch(trainpfam,task_num,k_spt,k_qry,train_dict):
+    batch_pfam_idx = random.sample(range(len(trainpfam)), k = task_num)
+    batch = []
+    for idx in batch_pfam_idx:
+        pfam_sampled = trainpfam[idx]
+        spt =  sample_balanced('spt',k_spt,pfam_sampled,train_dict)
+        qry =  sample_balanced('qry',k_qry,pfam_sampled,train_dict)
+        batch.append({'spt':spt,'qry':qry})
+    return batch
+
+
+def mix_npfam(trainpfam, mix_n, k_spt, k_qry, train_dict):
+    batch_pfam_idx = random.sample(range(len(trainpfam)), k=mix_n)
+    spts = []
+    qrys = []
+    for idx in batch_pfam_idx:
+        pfam_sampled = trainpfam[idx]
+        spt = sample_balanced('spt', k_spt, pfam_sampled, train_dict)
+        qry = sample_balanced('qry', k_qry, pfam_sampled, train_dict)
+        spts.append(spt)
+        qrys.append(qry)
+    #         batch.append({'spt':spt,'qry':qry})
+    SPT = pd.concat(spts).sample(frac=1)
+    QRY = pd.concat(qrys).sample(frac=1)
+    mixset = {}
+    mixset['spt'] = SPT
+    mixset['qry'] = QRY
+
+    return mixset
+
+def sample_minibatch_mixpfam(trainpfam,task_num,mix_n,k_spt,k_qry,train_dict):
+    batch =[]
+    for i in range(task_num):
+        mixset = mix_npfam(trainpfam,mix_n,k_spt,k_qry,train_dict)
+        batch.append(mixset)
+    return batch
+# =======================================================
+#             model related
+# =======================================================
+def load_DISAE(cwd):
+    all_config = load_json(cwd + 'DTI_config.json')
+    albertconfig = AlbertConfig.from_pretrained(cwd+ all_config['DISAE']['albertconfig'])
+    m = AlbertForMaskedLM(config=albertconfig)
+    m = load_tf_weights_in_albert(m, albertconfig, cwd+all_config['DISAE']['albert_pretrained_checkpoint'])
+    prot_descriptor = m.albert
+    prot_tokenizer = BertTokenizer.from_pretrained(cwd+all_config['DISAE']['albertvocab'])
+    return  prot_descriptor,prot_tokenizer
+
+def get_repr_DTI_MAML(batch_data_pertask,mode,prot_tokenizer,chem_dict,protein_dict,args):
+    batch_chem_graphs_spt,batch_protein_tokenized_spt = get_repr_DTI(batch_data_pertask[mode],
+                                                                    prot_tokenizer,
+                                                                    chem_dict,protein_dict,
+                                                                    'DISAE')
+    batch_chem_graphs_spt = batch_chem_graphs_spt.to(args['device'])
+    batch_protein_tokenized_spt = batch_protein_tokenized_spt.to(args['device'])
+
+    y_spt = torch.LongTensor(list(batch_data_pertask[mode]['Activity'].values)).to(args['device'])
+    return batch_chem_graphs_spt,batch_protein_tokenized_spt,y_spt
+
+def core_batch_prediction_MAML(batch_data_pertask, args, tokenizer,mode,loss_fn,
+                               chem_dict, protein_dict, model,detach=False):
+
+    batch_chem_graphs_spt, batch_protein_tokenized_spt, y_spt = get_repr_DTI_MAML(batch_data_pertask,
+                                                              mode,
+                                                              tokenizer, chem_dict, protein_dict,
+                                                              args)
+
+    batch_logits = model(batch_protein_tokenized_spt, batch_chem_graphs_spt)
+    loss = loss_fn(batch_logits,y_spt)
+    if detach == True:
+        batch_logits = batch_logits.detach().cpu()
+        y_spt = y_spt.detach().cpu()
+
+    return batch_logits, y_spt,loss
+def get_repr_DTI_0shot(batch_data,tokenizer,chem_dict,protein_dict,args):
+    #  . . . .  chemicals  . . . .
+    chem_smiles = chem_dict[batch_data['InChIKey'].values.tolist()].values.tolist()
+    chem_graph_list = []
+    for smiles in chem_smiles:
+        mol = Chem.MolFromSmiles(smiles)
+        graph = mol_to_graph_data_obj_simple(mol)
+        chem_graph_list.append(graph)
+    chem_graphs_loader = DataLoader(chem_graph_list, batch_size=batch_data.shape[0],
+                                    shuffle=False)
+    for batch in chem_graphs_loader:
+        chem_graphs = batch
+    #  . . . .  proteins  . . . .
+    # if prot_descriptor_choice =='DISAE':
+    uniprot_list = batch_data['uniprot+pfam'].values.tolist()
+    protein_tokenized = torch.tensor([tokenizer.encode(protein_dict[uni]) for uni in uniprot_list  ])
+    #  . . . .  to return  . . . .
+    chem_graphs = chem_graphs.to(args['device'])
+    protein_tokenized = protein_tokenized.to(args['device'])
+    label = torch.LongTensor(list(batch_data['Activity'].values)).to(args['device'])
+    return chem_graphs, protein_tokenized,label
+
+def core_batch_prediction_0shot(batch_data_pertask, args, tokenizer,loss_fn,
+                               chem_dict, protein_dict, model,detach=True):
+    batch_chem_graphs_spt, batch_protein_tokenized_spt, y_spt = get_repr_DTI_0shot(batch_data_pertask,
+                                                              tokenizer, chem_dict, protein_dict,
+                                                              args)
+
+    batch_logits = model(batch_protein_tokenized_spt, batch_chem_graphs_spt)
+    loss = loss_fn(batch_logits,y_spt)
+    if detach == True:
+        batch_logits = batch_logits.detach().cpu()
+        y_spt = y_spt.detach().cpu()
+    return batch_logits, y_spt,loss
+
+def evaluate_binary_predictions(label, predprobs):
+    probs = np.array(predprobs)
+    predclass = np.argmax(probs, axis=1)
+    # --------------------------by label---
+    bothF1 = metrics.f1_score(label, predclass, average=None)
+    bothprecision = metrics.precision_score(label, predclass, average=None)
+    bothrecall = metrics.recall_score(label, predclass, average=None)
+    class0 = [bothF1[0], bothprecision[0], bothrecall[0]]
+    class1 = [bothF1[1], bothprecision[1], bothrecall[1]]
+    # -------------------------overall---
+    f1 = metrics.f1_score(label, predclass, average='weighted')
+    fpr, tpr, thresholds = metrics.roc_curve(label, probs[:, 1], pos_label=1)
+    auc = metrics.auc(fpr, tpr)
+    prec, reca, thresholds = metrics.precision_recall_curve(label, probs[:, 1], pos_label=1)
+    aupr = metrics.auc(reca, prec)
+    # overall ={'f1':f1,'auc':auc,'aupr':aupr}
+    overall = [f1,auc,aupr]
+    return overall,class0,class1
+
+
+# def evaluate(df, all_config, tokenizer, chem_dict, protein_dict, model,mode,loss_fn, datatype='dev'):
+#     # collected_logits = []
+#     # collected_labels = []
+#     # for i in range(int(df.shape[0] / all_config['batch_size'])):
+#         # print(i)
+#     batch_logits, batch_labels,loss = core_batch_prediction_MAML(df,  all_config, tokenizer,mode,
+#                                                             loss_fn,
+#                                                             chem_dict,
+#                                                        protein_dict, model,
+#                                                        detach=True)
+#         # collected_logits.append(batch_logits)
+#         # collected_labels.append(batch_labels)
+#     # collected_labels = np.concatenate(collected_labels, axis=0)
+#     # collected_logits = np.concatenate(collected_logits, axis=0)
+#     metric = evaluate_binary_predictions(collected_labels, collected_logits)
+#     # print("{}\t{:.5f}\t{:.5f}\t{:.5f}".format(datatype, metric[0], metric[1], metric[2]))
+#
+#     return metric
+
+
+# def deal_Nonetype_parm(grad,model_param_shape):
+#     #  deal with possible Nonetype param
+#     n = 0
+#     grad_list = []
+#     for g in grad:
+#         if type(g) == type(None):
+#             print(n,' is Nonetype')
+#             grad_list.append(torch.zeros(model_param_shape[n]))
+#         else:
+#             grad_list.append(g)
+#         n = n + 1
+#     return grad_list
+# #
+# def check_model_shape(model):
+#     n = 0
+#     model_param_shape = []
+#     for name, param in model.named_parameters():
+#         n += 1
+#         model_param_shape.append(param.shape)
+#     return model_param_shape
